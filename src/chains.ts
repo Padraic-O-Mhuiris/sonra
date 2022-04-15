e608db826595a28519e44b199d4911aa96af14bd
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-04-15 14:59:51 +0100

    update

--- src/chains.ts
+++ src/chains.ts
@@ -3,8 +3,8 @@ import { NativeCurrencyDefinitionSchema } from './currency'
 
 const ChainNameSchema = z.nativeEnum({
   MAINNET: 'mainnet',
-  // RINKEBY: "rinkeby",
-  // GOERLI: "goerli",
+  RINKEBY: 'rinkeby',
+  GOERLI: 'goerli',
   KOVAN: 'kovan',
 } as const)
 
@@ -12,8 +12,8 @@ export const chainName = ChainNameSchema.enum
 
 export const ChainIdSchema = z.nativeEnum({
   [chainName.MAINNET]: 1,
-  // [chainName.RINKEBY]: 4,
-  // [chainName.GOERLI]: 5,
+  [chainName.RINKEBY]: 4,
+  [chainName.GOERLI]: 5,
   [chainName.KOVAN]: 42,
 } as const)
 
@@ -26,12 +26,13 @@ export const ChainDefinitionSchema = z.object({
   chainId: ChainIdSchema,
   chainName: ChainNameSchema,
   currency: NativeCurrencyDefinitionSchema,
-  rpcUrls: z.array(z.string().url()).optional(),
-  blockExplorerUrls: z.array(z.string().url()).optional(),
 })
 
 export type ChainDefinition = z.infer<typeof ChainDefinitionSchema>
 
-export const ChainDefinitionsSchema = z.record(ChainNameSchema, ChainDefinitionSchema)
+export const ChainDefinitionsSchema = z.record(
+  ChainNameSchema,
+  ChainDefinitionSchema,
+)
 
 export type ChainDefinitions = z.infer<typeof ChainDefinitionsSchema>
