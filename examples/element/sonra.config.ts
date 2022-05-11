1e1ee76bb53196d82f6835d138c1db2f12be6db1
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-05-11 13:50:18 +0100

    fixups

--- examples/element/sonra.config.ts
+++ examples/element/sonra.config.ts
@@ -1,6 +1,7 @@
 import { ethers } from 'ethers'
 import { z } from 'zod'
 import { SonraFetch, SonraConfig, zx, addressify, Address } from '../../src'
+import 'tsconfig-paths/register'
 
 import {
   ERC20__factory,
--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env ts-node
 import { parse as commandLineArgs } from 'ts-command-line-args'
 import { SonraModel, SonraConfig, run } from './'
 import path from 'path'
